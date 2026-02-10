db4f0d9bcbc679cf6ed24689111e854377309e3b
Shreyas-Ravath <121306379+Shreyas-Ravath@users.noreply.github.com>
2026-02-10 11:02:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,34 @@ resource "aws_security_group" "test_sg" {
     Name = "intentops-test-sg"
   }
 }
-
+#testdemo-2
+resource "aws_security_group" "test_sg2" {
+  name        = "intentops-test-sg"
+  description = "Test security group for IntentOps demo"
+  
+  ingress {
+    description = "SSH from restricted ranges"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Modified by IntentOps to fix INTENT-NET-001
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "intentops-test-sg2"
+  }
+}
 output "security_group_id" {
   value = aws_security_group.test_sg.id
+}
+
+output "security_group_id-2" {
+  value = aws_security_group.test_sg2.id
 }
